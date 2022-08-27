240bfb6c87f63996c4d9bcd8d98f123494a0d0c3
Daniel Vaswani <vaswanidaniel@gmail.com>
2022-08-27 17:53:35 +0200

    working todos swapped out for db with trpc and prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,41 @@
+import { Todo } from "@prisma/client";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { MouseEvent, useState } from "react";
 import { trpc } from "../utils/trpc";
 
 type TodoCardProps = {
-  description: string;
-  isComplete: boolean;
-  callback?: Function;
+  todo: Todo;
+  // handleClick: Function;
 };
 
-const TodoCardArray: Array<TodoCardProps> = [
-  { description: "Do chores", isComplete: false },
-  { description: "Eat shit", isComplete: false },
-  { description: "Watch TV", isComplete: false },
-  { description: "Drink METH", isComplete: false },
-  { description: "Destroy cars", isComplete: false },
-  { description: "Have Fun", isComplete: false },
-  { description: "Fail", isComplete: false },
-];
+// const TodoArray: Array<Todo> = [
+//   { description: "Do chores", isComplete: false },
+//   { description: "Eat shit", isComplete: false },
+//   { description: "Watch TV", isComplete: false },
+//   { description: "Drink METH", isComplete: false },
+//   { description: "Destroy cars", isComplete: false },
+//   { description: "Have Fun", isComplete: false },
+//   { description: "Fail", isComplete: false },
+// ];
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-  const [todos, setTodos] = useState(TodoCardArray);
+  // const TodoArray =  ?? [];
+  // console.log(TodoArray);
+  const todosQuery = trpc.useQuery(["todo.getAll"]);
+  console.log("todos data is", todosQuery.data);
+  // const [todos, setTodos] = useState(todosData ? [...todosData] : []);
 
-  const toggleTodo = (atIndex: number) => {
-    setTodos(
-      todos.map((todo, index) =>
-        // Here you accept a id argument to the function and replace it with hard coded 🤪 2, to make it dynamic.
-        index === atIndex
-          ? { ...todo, isComplete: !todo.isComplete }
-          : { ...todo }
-      )
-    );
-  };
+  // const toggleTodo = (atIndex: number) => {
+  //   setTodos(
+  //     todos!.map((todo, index) =>
+  //       // Here you accept a id argument to the function and replace it with hard coded 🤪 2, to make it dynamic.
+  //       index === atIndex - 1
+  //         ? { ...todo, isComplete: !todo.isComplete }
+  //         : { ...todo }
+  //     )
+  //   );
+  // };
 
   return (
     <>
@@ -43,32 +46,36 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="container mx-auto flex select-none flex-col gap-2 items-center justify-center min-h-screen p-4">
-        {todos.map((todoItem, index) => {
-          return (
-            // <div onClick={() => toggleTodo(index)}>
-            <TodoCard
-              description={todoItem.description}
-              isComplete={todoItem.isComplete}
-              key={index}
-              callback={() => toggleTodo(index)}
-            />
-            // </div>
-          );
-        })}
+        {todosQuery.data ? (
+          todosQuery.data.map((todoItem) => {
+            return (
+              // <div onClick={() => toggleTodo(index)}>
+              <TodoCard
+                todo={todoItem}
+                key={todoItem.id}
+                // handleClick={() => toggleTodo(todoItem.id)}
+              />
+              // </div>
+            );
+          })
+        ) : (
+          <></>
+        )}
       </main>
     </>
   );
 };
 
-const TodoCard = ({ description, isComplete, callback }: TodoCardProps) => {
+const TodoCard = ({ todo }: TodoCardProps) => {
+  //, handleClick
   return (
     <section
       className="flex flex-row justify-between items-center w-96 p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105"
-      onClick={() => callback!()}
+      // onClick={() => handleClick()}
     >
-      <h2 className="text-lg text-gray-700">{description}</h2>
+      <h2 className="text-lg text-gray-700">{todo.description}</h2>
       <p className="text-sm text-gray-600">
-        is complete: {isComplete ? "true" : "false"}
+        is complete: {todo.isComplete ? "true" : "false"}
       </p>
     </section>
   );
