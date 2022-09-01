0cf144252a01eca4c2028c4143339bdec5769ce6
Daniel Vaswani <vaswanidaniel@gmail.com>
2022-09-02 01:06:57 +0200

    moved TodoCard to its own component, got rid of unused imports

---
+++ src/components/TodoCard/TodoCard.tsx
@@ -0,0 +1,33 @@
+import { Todo } from "@prisma/client";
+
+type TodoCardProps = {
+  todo: Todo;
+  handleToggle: Function;
+  handleDelete: Function;
+};
+
+const TodoCard = ({ todo, handleToggle, handleDelete }: TodoCardProps) => {
+  //, handleClick
+  return (
+    <section
+      className="flex flex-row justify-between items-center w-96 p-6 duration-500 border-2 border-gray-500 relative rounded shadow-xl motion-safe:hover:scale-105"
+      onClick={() => handleToggle()}
+    >
+      <p
+        className="w-4 h-4 bg-red-600 text-white z-0 flex items-center justify-center rounded -m-1.5 absolute top-0 right-0 motion-safe:hover:scale-[2]"
+        onClick={(e) => {
+          handleDelete();
+          e.stopPropagation();
+        }}
+      >
+        X
+      </p>
+      <h2 className="text-lg text-gray-700">{todo.description}</h2>
+      <p className="text-sm text-gray-600">
+        is complete: {todo.isComplete ? "true" : "false"}
+      </p>
+    </section>
+  );
+};
+
+export default TodoCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,7 @@
-import { Todo } from "@prisma/client";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { MouseEvent, useState } from "react";
 import { trpc } from "../utils/trpc";
-
-type TodoCardProps = {
-  todo: Todo;
-  handleToggle: Function;
-  handleDelete: Function;
-};
+import TodoCard from "../components/TodoCard/TodoCard";
 
 // const TodoArray: Array<Todo> = [
 //   { description: "Do chores", isComplete: false },
@@ -84,30 +77,6 @@ const Home: NextPage = () => {
         )}
       </main>
     </>
-  );
-};
-
-const TodoCard = ({ todo, handleToggle, handleDelete }: TodoCardProps) => {
-  //, handleClick
-  return (
-    <section
-      className="flex flex-row justify-between items-center w-96 p-6 duration-500 border-2 border-gray-500 relative rounded shadow-xl motion-safe:hover:scale-105"
-      onClick={() => handleToggle()}
-    >
-      <p
-        className="w-4 h-4 bg-red-600 text-white z-0 flex items-center justify-center rounded -m-1.5 absolute top-0 right-0 motion-safe:hover:scale-[2]"
-        onClick={(e) => {
-          handleDelete();
-          e.stopPropagation();
-        }}
-      >
-        X
-      </p>
-      <h2 className="text-lg text-gray-700">{todo.description}</h2>
-      <p className="text-sm text-gray-600">
-        is complete: {todo.isComplete ? "true" : "false"}
-      </p>
-    </section>
   );
 };
 
