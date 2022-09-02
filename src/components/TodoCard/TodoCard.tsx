330bd1db737da8721d1b7ba738b4c5730a68935d
Daniel Vaswani <vaswanidaniel@gmail.com>
2022-09-02 22:13:23 +0200

    object hides with css and then it gets deleted

--- src/components/TodoCard/TodoCard.tsx
+++ src/components/TodoCard/TodoCard.tsx
@@ -1,4 +1,5 @@
 import { Todo } from "@prisma/client";
+import { useState } from "react";
 
 type TodoCardProps = {
   todo: Todo;
@@ -7,15 +8,19 @@ type TodoCardProps = {
 };
 
 const TodoCard = ({ todo, handleToggle, handleDelete }: TodoCardProps) => {
+  const [showTodo, setShowTodo] = useState(true);
   //, handleClick
   return (
     <section
-      className="flex flex-row justify-between items-center w-96 p-6 duration-500 border-2 border-gray-500 relative rounded shadow-xl motion-safe:hover:scale-105"
+      className={`${
+        showTodo ? "flex" : "hidden"
+      } flex-row justify-between items-center w-96 p-6 duration-500 border-2 border-gray-500 relative rounded shadow-xl motion-safe:hover:scale-105`}
       onClick={() => handleToggle()}
     >
       <p
         className="w-4 h-4 bg-red-600 text-white z-0 flex items-center justify-center rounded -m-1.5 absolute top-0 right-0 motion-safe:hover:scale-[2]"
         onClick={(e) => {
+          setShowTodo(!showTodo);
           handleDelete();
           e.stopPropagation();
         }}
